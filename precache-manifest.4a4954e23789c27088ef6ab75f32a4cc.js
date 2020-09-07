8a75f549565258b9d866ed59cdebd3e5e6a41dac
ozair <truemail785@gmail.com>
2020-09-08 02:00:48 +0500

    Updates

---
+++ precache-manifest.4a4954e23789c27088ef6ab75f32a4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e5097190dd7940309482d9f275f8b4",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "c70d1947b286e4bfe400",
+    "url": "/quiz-app/static/css/main.409323b6.chunk.css"
+  },
+  {
+    "revision": "8c5f867ce15d5ab0005f",
+    "url": "/quiz-app/static/js/2.271a5cbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.271a5cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70d1947b286e4bfe400",
+    "url": "/quiz-app/static/js/main.cfddfa46.chunk.js"
+  },
+  {
+    "revision": "04649c9e96fcedc27ca7",
+    "url": "/quiz-app/static/js/runtime-main.5725e834.js"
+  }
+]);
